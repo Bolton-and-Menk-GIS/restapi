562037e1ffb851951ba6f69b9231a2ad24a8f208
Caleb Mackey <caleb.mackey@gmail.com>
2015-05-19 11:44:45 -0500

    Update __init__.py

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -7,18 +7,18 @@
 #
 # Created:     10/29/2014
 # Copyright:   (c) calebma 2014
-# Licence:     GNU
+# Licence:     BMI
 #-------------------------------------------------------------------------------
-from rest_utils import *
+from rest_utils import FeatureService, FeatureLayer, GeocodeService, GPService, GPTask
 
 # look for arcpy access, otherwise use open source version
 # open source version may be faster.
 try:
     import imp
     imp.find_module('arcpy')
-    from arc_restapi import *
+    from arc_restapi import Cursor, MapService, MapServiceLayer, ArcServer, ImageService, Geocoder
 except ImportError:
-    from open_restapi import *
+    from open_restapi import Cursor, MapService, MapServiceLayer, ArcServer, ImageService, Geocoder
 
 # package info
 __author__ = 'Caleb Mackey'
@@ -26,5 +26,3 @@ __organization__ = 'Bolton & Menk, Inc.'
 __author_email__ = 'calebma@bolton-menk.com'
 __website__ = 'https://github.com/Bolton-and-Menk-GIS/restapi'
 __version__ = '0.1'
-
-
