71b21c275ebb240b453d9d2d3178d9bbaa9b627b
Caleb Mackey <caleb.mackey@gmail.com>
2015-07-13 13:07:28 -0500

    added exportReplica

--- restapi/arc_restapi.py
+++ restapi/arc_restapi.py
@@ -4,6 +4,8 @@ import arcpy
 import os
 import time
 import json
+import rest_utils
+reload(rest_utils)
 from rest_utils import *
 arcpy.env.overwriteOutput = True
 
@@ -26,6 +28,123 @@ def poly_to_json(poly, envelope=False):
     with arcpy.da.SearchCursor(poly, ['SHAPE@JSON']) as rows:
         for row in rows:
             return row[0].encode('utf-8')
+
+def exportReplica(replica, out_folder):
+    """converts a restapi.Replica() to a File Geodatabase
+
+    replica -- input restapi.Replica() object, must be generated from restapi.FeatureService.createReplica()
+    out_folder -- full path to folder location where new geodatabase will be stored.
+                The geodatabase will be named the same as the replica
+    """
+    if not hasattr(replica, 'replicaName'):
+        print 'Not a valid input!  Must be generated from restapi.FeatureService.createReplica() method!'
+        return
+
+    # attachment directory and gdb set up
+    att_loc = os.path.join(out_folder, 'Attachments')
+    if not os.path.exists(att_loc):
+        os.makedirs(att_loc)
+
+    out_gdb_name = arcpy.ValidateTableName(replica.replicaName, out_folder).split('.')[0] + '.gdb'
+    gdb = arcpy.management.CreateFileGDB(out_folder, out_gdb_name, 'CURRENT').getOutput(0)
+
+    # set schema and create feature classes
+    for layer in replica.layers:
+
+        # download attachments
+        att_dict = {}
+        for attInfo in layer.attachments:
+            out_file = os.path.join(att_loc, attInfo['name'])
+            with open(out_file, 'wb') as f:
+                f.write(urllib.urlopen(attInfo['url']).read())
+            att_dict[attInfo['parentGlobalId']] = out_file
+
+        fc = os.path.join(gdb, arcpy.ValidateTableName(layer.name, gdb))
+
+        arcpy.management.CreateFeatureclass(gdb, os.path.basename(fc), G_DICT[layer.geometryType],
+                                            spatial_reference=layer.spatialReference)
+
+        # set up schema
+        guid, guidFieldName = None, None
+        layer_fields = [f for f in layer.fields if f.type not in (SHAPE, OID)]
+        for i, field in enumerate(layer_fields):
+
+            if field.type == 'esriFieldTypeGlobalID':
+                field_name = 'ORIG_GlobalID'
+                guid = i
+                guidFieldName = field.name
+            else:
+                field_name = field.name
+
+            # set up domain if necessary
+            gdb_domains = []
+            if field.domain:
+                if field.domain['name'] not in gdb_domains:
+                    if 'codedValues' in field.domain:
+                        dType = 'CODED'
+                    else:
+                        dType = 'RANGE'
+
+                    arcpy.management.CreateDomain(gdb, field.domain['name'],
+                                                  field.domain['name'],
+                                                  FTYPES[field.type],
+                                                  dType)
+                    if dType == 'CODED':
+                        for cv in field.domain['codedValues']:
+                            arcpy.management.AddCodedValueToDomain(gdb, field.domain['name'], cv['code'], cv['name'])
+                    else:
+                        _min, _max = field.domain['range']
+                        arcpy.management.SetValueForRangeDomain(gdb, field.domain['name'], _min, _max)
+
+                    gdb_domains.append(field.domain['name'])
+
+                field_domain = field.domain['name']
+            else:
+                field_domain = ''
+
+            arcpy.management.AddField(fc, field_name, FTYPES[field.type],
+                                        field_length=field.length,
+                                        field_alias=field.alias,
+                                        field_domain=field_domain)
+
+        # set up field values
+        fld_names = ['SHAPE@'] + [f.name for f in layer_fields]
+        if guid != None:
+            fld_names[guid + 1] = 'ORIG_GlobalID'
+        date_indices = [i for i,f in enumerate(layer_fields) if f.type == 'esriFieldTypeDate']
+
+        with arcpy.da.InsertCursor(fc, fld_names) as irows:
+            for rowD in layer.features:
+                row = [rowD['attributes'][f] if f in rowD['attributes']
+                       else rowD['attributes'][guidFieldName] for f in fld_names[1:]]
+
+                for i in date_indices:
+                    row[i] = mil_to_date(row[i])
+
+                shape = arcpy.AsShape(rowD['geometry'], True)
+                irows.insertRow([shape] + row)
+
+        # Enable Attachments
+        if layer.attachments:
+            arcpy.management.AddGlobalIDs(fc)
+            arcpy.management.EnableAttachments(fc)
+
+            # create temp table
+            tmp_tab = r'in_memory\temp_photo_points'
+            arcpy.management.CreateTable('in_memory', 'temp_photo_points')
+            arcpy.management.AddField(tmp_tab, 'ORIG_GlobalID', 'TEXT')
+            arcpy.management.AddField(tmp_tab, 'PATH', 'TEXT', field_length=255)
+            arcpy.management.AddField(tmp_tab, 'PHOTO_NAME', 'TEXT', field_length=254)
+            with arcpy.da.InsertCursor(tmp_tab, ['ORIG_GlobalID', 'PATH', 'PHOTO_NAME']) as irows:
+                for k,v in att_dict.iteritems():
+                    irows.insertRow((k,) + os.path.split(v))
+
+            # add attachments
+            arcpy.management.AddAttachments(fc, 'ORIG_GlobalID', tmp_tab, 'ORIG_GlobalID', 'PHOTO_NAME', in_working_folder=att_loc)
+            arcpy.management.Delete(tmp_tab)
+
+    print 'Created: "{}"'.format(gdb)
+    return gdb
 
 class Cursor(BaseCursor):
     """Class to handle Cursor object"""
@@ -54,12 +173,12 @@ class Cursor(BaseCursor):
             yield Row(feature, self.field_objects)
 
     def rows(self):
-        """returns row values as tuple"""
+        """returns Cursor.rows() as generator"""
         for feature in self.features[:self.records]:
             yield Row(feature, self.field_objects).values
 
     def __iter__(self):
-        """returns Cursor.rows() generator"""
+        """returns Cursor.rows()"""
         return self.rows()
 
 class Row(BaseRow):
@@ -78,7 +197,7 @@ class Row(BaseRow):
         """returns arcpy geometry object
         Warning: output is unprojected
             use the projectAs(wkid, {transformation_name})
-            methedto project geometry
+            method to project geometry
         """
         if self.shape_field_ob:
             return arcpy.AsShape(self.esri_json, True)
@@ -237,6 +356,22 @@ class MapService(BaseMapService):
         """
         lyr = self.layer(layer_name)
         lyr.layer_to_fc(out_fc, sr, where, params, flds, records, get_all)
+
+    def layer_to_kmz(self, layer_name, out_kmz='', flds='*', where='1=1', params={}):
+        """Method to create kmz from query
+
+        Required:
+            layer_name -- name of map service layer to export to fc
+
+        Optional:
+            out_kmz -- output kmz file path, if none specified will be saved on Desktop
+            flds -- list of fields for fc. If none specified, all fields are returned.
+                Supports fields in list [] or comma separated string "field1,field2,.."
+            where -- optional where clause
+            params -- dictionary of parameters for query
+        """
+        lyr = self.layer(layer_name)
+        lyr.layer_to_kmz(flds, where, params, kmz=out_kmz)
 
     def clip(self, layer_name, poly, output, fields='*', out_sr='', where='', envelope=False):
         """Method for spatial Query, exports geometry that intersect polygon or
