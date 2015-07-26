30050df9e9d24a69631613085f35f8c6d172f98d
Caleb Mackey <caleb.mackey@gmail.com>
2015-07-25 20:42:12 -0500

    Update __init__.py

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -16,9 +16,11 @@ from rest_utils import FeatureService, FeatureLayer, GeocodeService, GPService, 
 try:
     import imp
     imp.find_module('arcpy')
-    from arc_restapi import Cursor, MapService, MapServiceLayer, ArcServer, ImageService, Geocoder
+    from arc_restapi import Cursor, MapService, MapServiceLayer, ArcServer, \
+                            ImageService, Geocoder, exportFeatureSet, exportReplica
 except ImportError:
-    from open_restapi import Cursor, MapService, MapServiceLayer, ArcServer, ImageService, Geocoder
+    from open_restapi import Cursor, MapService, MapServiceLayer, ArcServer, \
+                             ImageService, Geocoder, exportFeatureSet, exportReplica
 
 # package info
 __author__ = 'Caleb Mackey'
@@ -26,9 +28,11 @@ __organization__ = 'Bolton & Menk, Inc.'
 __author_email__ = 'calebma@bolton-menk.com'
 __website__ = 'https://github.com/Bolton-and-Menk-GIS/restapi'
 __version__ = '0.1'
+__package__ = 'restapi'
 __documentation__ = 'http://gis.bolton-menk.com/restapi-documentation/restapi-module.html'
 
 def getHelp():
+    """call this function to open help documentation in a new tab"""
     import webbrowser
     webbrowser.open_new_tab(__documentation__)
     return __documentation__
