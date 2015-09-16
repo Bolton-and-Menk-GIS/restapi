38f0eac5007ab2ce292d594c923f47e8bf5cc337
Caleb Mackey <caleb.mackey@gmail.com>
2015-09-16 13:44:27 -0500

    setup and manifest update

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='restapi',
-      version='1.0',
+      version='0.1',
       description='Package for working with ArcGIS REST API',
       author='Caleb Mackey',
       author_email='calebma@bolton-menk.com',
       url='https://github.com/Bolton-and-Menk-GIS/restapi',
       license='GPL',
       packages=['restapi', 'restapi.admin'],
-      )
+      include_package_data=True,
+##      install_requires=['requests'],
+##      dependency_links=[
+##          'https://pypi.python.org/pypi/requests#downloads'
+##          ], #requests is now shipped with package
+      zip_safe=False)
 
 
