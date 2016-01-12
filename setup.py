2be3b7a35208acead7512b48f53ac805f3abe5e7
Caleb Mackey <caleb.mackey@gmail.com>
2016-01-12 10:08:32 -0600

    cleaned up setup.py
    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ name = 'restapi'
 pckgs = []
 for root,dirs,files in os.walk(name):
     if '__init__.py' in files:
-        rootBase = os.path.basename(root)
         pckgs.append(name + '.'.join(root.split(name)[-1].split(os.sep)))
         
 setup(name=name,
@@ -16,8 +15,8 @@ setup(name=name,
       url='https://github.com/Bolton-and-Menk-GIS/restapi',
       license='GPL',
       packages=pckgs,
-      package_dir={'restapi': 'restapi'},
-      package_data={'restapi': ['shapefile/*.json',
+      package_dir={name: name},
+      package_data={name: ['shapefile/*.json',
                                 'test/*.py',
                                 'admin/samples/*.py']},
       )
