ec483680f1acd78c1b81fa81ed3b2ccb5eb547e2
philna <phil.nagel@bolton-menk.com>
2019-10-28 21:24:14 -0500

    Setup mod

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from distutils.core import setup
+from setuptools import find_packages
 import os
 
-name = 'bmi-arc-restapi'
-pckgs = []
-for root,dirs,files in os.walk(name):
-    if '__init__.py' in files:
-        pckgs.append(name + '.'.join(root.split(name)[-1].split(os.sep)))
+name = 'bmi-arcgis-restapi'
         
 setup(name=name,
       version='0.1',
@@ -14,11 +11,10 @@ setup(name=name,
       author_email='calebma@bolton-menk.com',
       url='https://github.com/Bolton-and-Menk-GIS/restapi',
       license='GPL',
-      py_modules=pckgs,
-      packages=pckgs,
+      packages=['restapi'],
+      include_package_data=True,
       zip_Safe=False,
-      package_dir={name: name},
-      package_data={name: ['shapefile/*.json',
+      package_data={'restapi': ['shapefile/*.json',
                                 'test/*.py',
                                 'admin/samples/*.py']},
       install_requires=['munch', 'requests']
