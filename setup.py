5132099769ab804c6672910aeed9dd67f14ced31
philna <phil.nagel@bolton-menk.com>
2019-10-28 23:00:07 -0500

    Add README text to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import find_packages, setup
 #from distutils.core import setup
 import os
 
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 name = 'bmi-arcgis-restapi'
 
 setup(name=name,
-      version='0.1',
+      version='1.0',
       description='Package for working with ArcGIS REST API',
       author='Caleb Mackey',
       author_email='calebma@bolton-menk.com',
@@ -17,5 +21,7 @@ setup(name=name,
                                 'test/testData/*',
                                 'admin/samples/*.py',
                                 'projections/bin/*']},
-      install_requires=['munch', 'requests']
+      install_requires=['munch', 'requests'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
 )
