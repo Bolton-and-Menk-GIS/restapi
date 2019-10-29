166c549768c1f084b7b4234e2c45ae3d5759ad95
philna <phil.nagel@bolton-menk.com>
2019-10-29 12:15:18 -0500

    bugfix

--- setup.py
+++ setup.py
@@ -31,10 +31,10 @@ setup(name=name,
           'Natural Language :: English',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4'
-          'Programming Language :: Python :: 3.5'
-          'Programming Language :: Python :: 3.6'
-          'Programming Language :: Python :: 3.7'
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Topic :: Scientific/Engineering :: GIS'
       ]
 )
