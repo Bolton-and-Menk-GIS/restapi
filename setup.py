63e79e23083ee6dd1f6ba3c15b22b8932b462cd0
philna <phil.nagel@bolton-menk.com>
2020-09-24 11:41:16 -0500

    Add Py3.8 compatability tag

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(name=name,
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Topic :: Scientific/Engineering :: GIS'
       ]
 )
