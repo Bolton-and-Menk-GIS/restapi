b858b95ca60b198f2a08155c0449828c45096975
CalebM1987 <caleb.mackey@gmail.com>
2021-01-22 12:56:22 -0600

    build geometry- helper app

---
+++ restapi/geometry-helper/precache-manifest.aa24377ed2df2d99e76927b614ef8dee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7624f15612395320084e",
+    "url": "css/app.615af0e5.css"
+  },
+  {
+    "revision": "5087cc34731a5dda5f35",
+    "url": "css/chunk-26ed89da.8fb689c1.css"
+  },
+  {
+    "revision": "907132ccce02a33cd084",
+    "url": "css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "b4e2232cc8f926fb0a4e0d1b3c421d65",
+    "url": "index.html"
+  },
+  {
+    "revision": "7624f15612395320084e",
+    "url": "js/app.453ee291.js"
+  },
+  {
+    "revision": "5087cc34731a5dda5f35",
+    "url": "js/chunk-26ed89da.8c999e80.js"
+  },
+  {
+    "revision": "907132ccce02a33cd084",
+    "url": "js/chunk-vendors.b1f44c4e.js"
+  },
+  {
+    "revision": "17999de1069f5bbaa59c130a544665c6",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
